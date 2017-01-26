f79b0d642ad26fcef978e090e80e93edc6095819
Hao Yan <haoyan@wustl.edu>
2017-01-26 14:19:53 -0600

    merge file

--- twoMerge.py
+++ twoMerge.py
@@ -1,18 +1,26 @@
 import cPickle
-houseTypeTwoDict_1 = cPickle.load(open('housePressReleaseArticleUrlDict_type2_part1','rb'))
-houseTypeTwoDict_2 = cPickle.load(open('housePressReleaseArticleUrlDict_type2_part2','rb'))
-houseTypeTwpDict = {}
-for name, urlList in houseTypeTwoDict_1.iteritems():
-	if len(urlList) > 0:
-		houseTypeTwpDict[name] = urlList
-	elif len(houseTypeTwoDict_2[name]) > 0:
-		houseTypeTwpDict[name] = houseTypeTwoDict_2[name]
-	else:
-		houseTypeTwpDict[name] = []
+import sys
+DictFile_1 = sys.argv[1]
+DictFile_2 = sys.argv[2]
+OutFile_1 = sys.argv[3]
+OutFile_2 = sys.argv[4]
 
-cPickle.dump(houseTypeTwpDict,open('housePressReleaseArticleUrlDict_type1','wb'))
-writer = open('output_2.log','w')
-for name, urlList in houseTypeTwpDict.iteritems():
+newDictFile = sys.argv[5]
+newOutFile = sys.argv[6]
+houseDict_1 = cPickle.load(open(DictFile_1,'rb'))
+houseDict_2 = cPickle.load(open(DictFile_2,'rb'))
+houseDict = {}
+for name, urlList in houseDict_1.iteritems():
+	if len(urlList) > 0:
+		houseDict[name] = urlList
+	elif len(houseTypeTwoDict_2[name]) > 0:
+		houseDict[name] = houseDict_2[name]
+	else:
+		houseDict[name] = []
+
+cPickle.dump(houseDict,open(newDictFile,'wb'))
+writer = open(newOutFile,'w')
+for name, urlList in houseDict.iteritems():
 	writer.write(name+' , ')
 	if len(urlList) != 0:
 		writer.write(str(len(urlList))+' article found\n')
