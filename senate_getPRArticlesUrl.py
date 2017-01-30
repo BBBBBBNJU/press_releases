b7c647b70e75625b070160e68d07806bf21ae6de
BBBBBBNJU <haoyan@wustl.edu>
2017-01-30 17:37:49 -0600

    senate PR article url list

--- senate_getPRArticlesUrl.py
+++ senate_getPRArticlesUrl.py
@@ -14,25 +14,69 @@ import sys
 htmlentities = ["&quot;","&nbsp;","&amp;","&lt;","&gt;","&OElig;","&oelig;","&Scaron;","&scaron;","&Yuml;","&circ;","&tilde;","&ensp;","&emsp;","&thinsp;","&zwnj;","&zwj;","&lrm;","&rlm;","&ndash;","&mdash;","&lsquo;","&rsquo;","&sbquo;","&ldquo;","&rdquo;","&bdquo;","&dagger;","&Dagger;","&permil;","&lsaquo;"]
 
 def getunicodePage(Url):
-    page = requests.get(Url)
-    html_contents = page.text
-    unicodePage = ""
-    for eachElement in html_contents:
-    	try:
-    		eachElement.decode("ascii",'ignore')
-    		unicodePage += eachElement
-    	except:
-    		continue
-    return unicodePage
+	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	page = requests.get(Url,headers=headers)
+	html_contents = page.text
+	unicodePage = ""
+	for eachElement in html_contents:
+		try:
+			eachElement.decode("ascii",'ignore')
+			unicodePage += eachElement
+		except:
+			continue
+	return unicodePage
 
 def checkUrl(tempUrl):
-    response = urllib2.urlopen(tempUrl)
-    redirected = response.geturl() == tempUrl
-    return redirected,response.geturl()
+	response = urllib2.urlopen(tempUrl)
+	redirected = response.geturl() == tempUrl
+	return redirected,response.geturl()
 
+
+def oneTypeArticleUrlCrawler(articleUrlList, PR_homeUrl, senateHomeUrl, pageIndex):
+	try:
+		temp_url = PR_homeUrl + "?page=" + pageIndex
+		unicodePage=getunicodePage(temp_url)
+		if 'WEBSITE TEMPORARILY UNAVAILABLE DUE TO MAINTENANCE' in unicodePage:
+			unicodePage=getunicodePage(temp_url)
+		target='<td class="recordListTitle"><a href="(.*?)"'
+		myItems = re.findall(target,unicodePage,re.DOTALL)
+		tempUrlList = []
+		for eachitem in myItems:
+			tempUrlList.append(eachitem)
+		target='page=(.*?)">Next'
+		myItems = re.findall(target,unicodePage,re.IGNORECASE)
+		print myItems
+		if len(myItems) == 0:
+			return articleUrlList + tempUrlList
+		else:
+			return oneTypeArticleUrlCrawler(articleUrlList + tempUrlList, PR_homeUrl, senateHomeUrl, myItems[0])
+	except:
+		return articleUrlList
+
+
+chosen_type = sys.argv[1]
 senateUrlDict = cPickle.load(open('senateUrlDict','rb'))
+senatePressReleaseArticleUrlDict = {}
 for name, homeUrls in senateUrlDict.iteritems():
-	PR_homeUrl = homeUrls[1] + '/'
+	PR_homeUrl = homeUrls[1]
+	if not PR_homeUrl.endswith('/'):
+		PR_homeUrl += '/'
 	homeUrl = homeUrls[0]
+	if not homeUrl.endswith('/'):
+		homeUrl += '/'
+	
+	# tyep 1 
+	if 'index.cfm/' in PR_homeUrl and chosen_type == '1':
+		senateArticleUrlList = oneTypeArticleUrlCrawler([], PR_homeUrl[0:len(PR_homeUrl)-1], homeUrl , '1')
+		print name,
+		if len(senateArticleUrlList) == 0:
+			print ", no article list found"
+			os.system("echo \"Finish processing: "+ name + ", " + "type: "+ chosen_type +", no article list found\" | mail -s \"update\" haoyan.wustl@gmail.com")
+		else:
+			print ", " + str(len(senateArticleUrlList)) + ' article found'
+			os.system("echo \"Finish processing: "+ name + ", " + "type: "+ chosen_type + ", " + str(len(senateArticleUrlList)) + "article found\" | mail -s \"update\" haoyan.wustl@gmail.com")
+			senatePressReleaseArticleUrlDict[name] = senateArticleUrlList
+	cPickle.dump(senatePressReleaseArticleUrlDict,open('senatePressReleaseArticleUrlDict_type' + chosen_type,'wb'))
+
 
 
